bd8bcb4e18a3d44be9ae1f1246e9412131d84428
khatley <khatley.dev@gmail.com>
2025-03-28 13:12:14 -0600

    Added Remote Computer Support (First Attempt)

--- Create-VMs.ps1
+++ Create-VMs.ps1
@@ -29,7 +29,6 @@
 
 .EXAMPLE
     .\Create-VM.ps1 .\VM-Alpine-Parameters.json
-    TEST CHANGE TO CONFIRM GIT IS WORKING
 #>
 
 [CmdletBinding()]
@@ -46,6 +45,7 @@ if(Test-Path -Path $JSONPath){
 }
 
 foreach ($Config in $VMConfigs){
+    $ComputerName = $Config.ComputerName
     $VMName = $Config.VMName
     $ProcessorCount = [int]$Config.ProcessorCount
     $MemoryStartupBytes = [int]$Config.MemoryStartupBytes * 1GB
@@ -58,37 +58,37 @@ foreach ($Config in $VMConfigs){
     $Generation = 2 # 1 or 2 for UEFI support if the OS and host support it
 
     # --- Create Virtual Hard Disk ---
-    New-VHD -Path $VHDPath -SizeBytes $VHDSizeBytes
+    New-VHD -ComputerName $ComputerName -Path $VHDPath -SizeBytes $VHDSizeBytes
 
     # --- Create Virtual Machine ---
-    New-VM -Name $VMName -MemoryStartupBytes $MemoryStartupBytes -Generation $Generation
+    New-VM -Name $VMName -ComputerName $ComputerName -MemoryStartupBytes $MemoryStartupBytes -Generation $Generation
 
     # --- Configure Processor ---
-    Set-VMProcessor -VMName $VMName -Count $ProcessorCount
+    Set-VMProcessor -VMName $VMName -ComputerName $ComputerName -Count $ProcessorCount
 
     # --- Connect Network Adapter to Virtual Switch ---
-    Connect-VMNetworkAdapter -VMName $VMName -Name "Network Adapter" -SwitchName $SwitchName
+    Connect-VMNetworkAdapter -VMName $VMName -ComputerName $ComputerName -Name "Network Adapter" -SwitchName $SwitchName
 
     # --- Add DVD Drive and Mount ISO ---
-    Add-VMDvdDrive -VMName $VMName -Path $ISOPath
+    Add-VMDvdDrive -VMName $VMName -ComputerName $ComputerName -Path $ISOPath
 
     # --- Add Existing Hard Disk ---
-    Add-VMHardDiskDrive -VMName $VMName -Path $VHDPath
+    Add-VMHardDiskDrive -VMName $VMName -ComputerName $ComputerName -Path $VHDPath
 
     # --- Set Boot Order and Disable SecureBoot ---
-    $firmware = Get-VMFirmware -VMName $VMName
+    $firmware = Get-VMFirmware -VMName $VMName -ComputerName $ComputerName
     $network = $firmware.bootorder[0]
     $dvd = $firmware.bootorder[1]
     $vhd = $firmware.bootorder[2]
 
-    Set-VMFirmware -VMName $VMName -FirstBootDevice $dvd
-    Set-VMFirmware -VMName $VMName -EnableSecureBoot Off
+    Set-VMFirmware -VMName $VMName -ComputerName $ComputerName -FirstBootDevice $dvd
+    Set-VMFirmware -VMName $VMName -ComputerName $ComputerName -EnableSecureBoot Off
 
     # --- Start the Virtual Machine ---
     Write-Host "Starting virtual machine '$VMName'..."
-    Start-VM -Name $VMName
+    Start-VM -Name $VMName -ComputerName $ComputerName
 
     # --- Set permant boot order ---
-    Set-VMFirmware -VMName $VMName -BootOrder $vhd, $dvd, $network
+    Set-VMFirmware -VMName $VMName -ComputerName $ComputerName -BootOrder $vhd, $dvd, $network
     Write-Host "Virtual machine '$VMName' created and started. The OS installation should begin."
 }
